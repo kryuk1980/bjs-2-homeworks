f4af4e995d4852cb08ede9935bd9c56cb5a5a4ef
kryuk1980 <kruyk@bk.ru>
2023-04-20 20:57:25 +0300

    Задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,57 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+
+    let cache = [];
+
+    function wrapper(...args) {
+
+        let wrapHash = md5(args);
+        let objectInCache = cache.findIndex((item, index, arr) => arr[index].hash === wrapHash);
+
+        if (objectInCache > -1) {
+            return 'Из кэша: ' + cache[objectInCache].value;
+        }
+
+        let resultFunc = func(...args);
+        let newItem = {
+            hash: wrapHash,
+            value: resultFunc
+        }
+
+        cache.push(newItem);
+
+        if (cache.length > 5) {
+            cache.shift();
+        }
+
+        return 'Вычисляем: ' + resultFunc;
+
+    }
+
+    return wrapper;
+
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    function wrapper(...args) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        if (timeoutId === null) {
+            func(...args);
+            wrapper.count++;
+        }
+        timeoutId = setTimeout(() => {
+            timeoutId = true;
+            func(...args);
+            wrapper.count++;
+        }, delay)
+        wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+
 }
